5ff60f7257f064a41bc90066230632472cccc7a3
Renato <renato.salvador@ua.pt>
2018-10-10 20:54:25 +0100

    fgdsgfh

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 
 var climate = require('city-weather');
 var fs = require('fs');
-
+const OpenWeatherMapHelper = require("openweathermap-node");
+const helper = new OpenWeatherMapHelper(
+    {
+        APPID: '9ba93df184746ae637c18d8119b8cce0',
+        units: "metric"
+    }
+);
 
 module.exports = (app) => {
 
@@ -13,57 +19,52 @@ module.exports = (app) => {
             weather
         } = body;
 
-        async function f() {
+ f();
 
+        async function f() {
             let promise = new Promise((resolve, reject) => {
                 var data = {};
                 for (let i = 0; i < weather.length; i++) {
                     if (!data[weather[i]]) {
                         data[weather[i]] = {};
                     }
-                    climate.getMaximumTemp(weather[i], (temp) => {
-                        console.log("Maximum temperature: " + temp);
-                        data[weather[i]]['TempMax'] = temp;
-                    });
 
-                    climate.getMinimumTemp(weather[i], function (temp) {
-                        data[weather[i]]['TempMin'] = temp;
-                    });
-
-                    climate.getActualTemp(weather[i], function (temp) {
-                        data[weather[i]]['AtualTemp'] = temp;
-                    });
-
-                    climate.getClimateDescription(weather[i], function (description) {
-                        data[weather[i]]['Description'] = description;
-                    });
-
-                    climate.getWindSpeed(weather[i], (speed) => {
-                        data[weather[i]]['Wind'] = speed;
-
+                    helper.getCurrentWeatherByCityName(weather[i], (err, currentWeather) => {
+                        if (err) {
+                            reject(err)
+                        }
+                        else {
+                            console.log(currentWeather,'API RETURN')
+                            data[weather[i]]['AtualTemp'] = currentWeather.main.temp;
+                            data[weather[i]]['TempMax'] = currentWeather.main.temp_max;
+                            data[weather[i]]['TempMin'] = currentWeather.main.temp_min;
+                            data[weather[i]]['Description'] = currentWeather.weather[0].description;
+                            data[weather[i]]['Wind'] = currentWeather.wind.speed;
+                        }
                     });
                 }
                 setTimeout(() => resolve(data), 1000)
-            });
 
-            let result = await promise; // wait till the promise resolves (*)
-
-            fs.writeFile("temp.txt", JSON.stringify(result), (err) => {       //// Bloco de notas
-                if (err) {
-                    console.log(err);
-                }
+            }).then((value) => {
+                console.log(value, 'value')
+                fs.appendFile("temp.txt", JSON.stringify(value), (err) => {       //// Bloco de notas
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log('Saved Log!')
+                });
+        
+                res.send({
+                    success: true,
+                    data: value
+                })
+            }).catch((err) => {
+                console.log(err)
+                res.send({
+                    success: false,
+                    err: err
+                })
             });
-            res.send({
-                success: true,
-                data: result
-            })
-            console.log(result); // "done!"
         }
-
-        f();
-
-
     })
-
-
 }
